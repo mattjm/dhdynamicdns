7f1861a200180419bc9afcee7b8a90ce809ef697
Matt MacAdam <mattjm@uw.edu>
2015-02-18 19:52:01 -0800

    convert to python2

--- dns.py
+++ dns.py
@@ -1,4 +1,5 @@
-#!/home/mattjm/bin/python3
+#!/home/mattjm/local/bin
+
 import json
 import httpcalls
 import datetime
@@ -13,19 +14,19 @@ def main():
     
     #-----USER EDITABLE BELOW-----
     #Specify API Key here
-    apiKey='ABC'
+    apiKey=u'ABC'
     #specify domain name here
-    domainName = 'domain.example.com'
+    domainName = u'domain.example.com'
     #specify password below (NOT A PASSWORD YOU USE FOR ANYTHING ELSE)
     #This is just for this tool
-    myPass = 'oneseventhree'
+    myPass = u'oneseventhree'
     #-----USER EDITABLE ABOVE-----
     
     #this lets a web browser display the output of the program
-    print("Content-type: text/html\n\n")
+    print u"Content-type: text/html\n\n"
     
     #the API URL shouldn't change, but if it does you can change it here
-    apiURL = 'https://api.dreamhost.com/'
+    apiURL = u'https://api.dreamhost.com/'
     
    
     #get the current date....ignore the 2015-01-01....for some reason
@@ -38,23 +39,23 @@ def main():
     myDAL = httpcalls.DAL(baseURL=apiURL)
     
     #Magic to obtain external IP address
-    currentlocalIP=os.environ['REMOTE_ADDR']
-    print(currentlocalIP)
+    currentlocalIP=os.environ[u'REMOTE_ADDR']
+    print currentlocalIP
     #Get password from query string via server variables
     myData = cgi.FieldStorage()
-    givenPass = myData.getfirst('password','')
+    givenPass = myData.getfirst(u'password',u'')
     #check password
     if givenPass != myPass:
-        print('Unauthorized')
+        print u'Unauthorized'
         exit()
 
     
     
     #This dictionary will be turned into a querystring by the requests library.  It contains
     #all the required parameters for the API call
-    myListQuery = {'key':apiKey,
-              'cmd':'dns-list_records',
-              'format':'json'}
+    myListQuery = {u'key':apiKey,
+              u'cmd':u'dns-list_records',
+              u'format':u'json'}
     
     #get the list of current records
     mydhRecords = myDAL.rqGET(values=myListQuery)
@@ -64,24 +65,24 @@ def main():
     
     #create some variables--we'll set them in the loop that follows
     shouldChange = True
-    oldIP = ''
+    oldIP = u''
     recordExists = False 
     #iterate through all records
-    for x in allRecords['data']:
+    for x in allRecords[u'data']:
         #if we find the record we want, let's do some stuff with it
-        if x['record'] == domainName and not recordExists:
+        if x[u'record'] == domainName and not recordExists:
             #Let's make a note that the record exists
             recordExists = True
             #check the IP address in the record against the one we have right now
-            if x['value'] == currentlocalIP:
+            if x[u'value'] == currentlocalIP:
                 #we don't need to change it if it's the same
                 shouldChange = False
-                print('IP has not changed--no update needed.')
+                print u'IP has not changed--no update needed.'
             else:
                 #If they aren't the same we need to know the old IP to delete the outdated record
                 #remember shouldChange was true when we created it so we don't need to change the flag
-                oldIP = x['value']
-                print('IP has changed--will proceed with update')
+                oldIP = x[u'value']
+                print u'IP has changed--will proceed with update'
             
             
     #Only do the following if the IP isn't the same as it was before
@@ -91,38 +92,38 @@ def main():
         
         #This dictionary will be turned into a querystring by the requests library.  It contains
         #all the required parameters for the API call
-        myoldrecord = {'record':domainName,
-                       'type':'A',
-                       'value':oldIP,
-                       'key':apiKey,
-                       'cmd':'dns-remove_record',
-                       'format':'json'}
+        myoldrecord = {u'record':domainName,
+                       u'type':u'A',
+                       u'value':oldIP,
+                       u'key':apiKey,
+                       u'cmd':u'dns-remove_record',
+                       u'format':u'json'}
         
         
         #delete the outdated record if it exists
         if recordExists:
             myrecordedit = myDAL.rqGET(values=myoldrecord)
             #print the results
-            print(myrecordedit.text)
+            print myrecordedit.text
         
         #Create new record
         #This dictionary will be turned into a querystring by the requests library.  It contains
         #all the required parameters for the API call
-        mynewrecord = {'record':'pytest.mattjm.com',
-                       'type':'A',
-                       'value':currentlocalIP,
-                       'comment' : 'updated by mattjm python script at ' + str(myDate), 
-                       'key':apiKey,
-                       'cmd':'dns-add_record',
-                       'format':'json'}
+        mynewrecord = {u'record':u'pytest.mattjm.com',
+                       u'type':u'A',
+                       u'value':currentlocalIP,
+                       u'comment' : u'updated by mattjm python script at ' + unicode(myDate), 
+                       u'key':apiKey,
+                       u'cmd':u'dns-add_record',
+                       u'format':u'json'}
         
        
         
         #add the record
         myrecordedit = myDAL.rqGET(values=mynewrecord)
         #print the results
-        print(myrecordedit.text)
+        print myrecordedit.text
    
         
 
-if __name__ == '__main__':  main()
+if __name__ == u'__main__':  main()
